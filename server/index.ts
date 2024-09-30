b06e5c1ade50dc94f55f08d1c318515853f3692a
Kevin Leung <kevin.leung@nagrastar.com>
2024-09-29 23:37:33 -0600

    feat: initialize server

---
+++ server/index.ts
@@ -0,0 +1,24 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+
+app.get("/", (req: Request, res: Response) => {
+  console.log("hello world me!");
+  res.send("Express + TypeScript Server");
+});
+
+app.get("/health", (req: Request, res: Response) => {
+  console.log("hello health check");
+  res.send("I am healthy");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
